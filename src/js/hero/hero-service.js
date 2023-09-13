3961444f42af37b1f814c9f29f475ac5c29d8190
chertik77 <babychdenya@gmail.com>
2023-09-13 19:28:12 +0100

    complete validation for hero-modal

--- src/js/hero/hero-service.js
+++ src/js/hero/hero-service.js
@@ -1,4 +1,41 @@
 import { disablePageScroll, enablePageScroll } from 'scroll-lock';
+import JustValidate from 'just-validate';
+
+const form = document.querySelector('#order-form');
+const { name, phone, email } = form.elements;
+const validator = new JustValidate('#order-form', { validateBeforeSubmitting: true });
+
+validator.addField('#name-input', [
+  {
+    rule: 'required',
+  },
+  {
+    rule: 'minLength',
+    value: 3,
+  },
+  {
+    rule: 'maxLength',
+    value: 15,
+  },
+]);
+
+validator.addField('#phone-input', [
+  {
+    rule: 'required',
+  },
+  {
+    rule: 'customRegexp',
+    value: '[789][0-9]{9}',
+  },
+]);
+validator.addField('#email-input', [
+  {
+    rule: 'required',
+  },
+  {
+    rule: 'email',
+  },
+]);
 
 const modalWindow = document.querySelector('#ordernow');
 
@@ -12,8 +49,12 @@ document.querySelector('.btn-close').addEventListener('click', () => {
   enablePageScroll();
 });
 
-document.querySelector('.modal-order-send').addEventListener('click', e => {
-  e.preventDefault();
-  modalWindow.close();
-  enablePageScroll();
+document.querySelector('.modal-order-send').addEventListener('click', () => {
+  if (!name.value || !phone.value || !email.value) {
+    return;
+  } else {
+    modalWindow.close();
+    enablePageScroll();
+    form.reset();
+  }
 });
