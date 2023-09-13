066532d496be3808aaf0b31a8f1c15358a977f3d
chertik77 <babychdenya@gmail.com>
2023-09-13 10:32:19 +0100

    create-hero

--- src/js/hero-swiper.js
+++ src/js/hero-swiper.js
@@ -1,4 +1,13 @@
-// import Splide from '@splidejs/splide';
-// import '@splidejs/splide/css';
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
-// new Splide('.splide').mount();
+document.addEventListener('DOMContentLoaded', () => {
+  new Splide('.splide', {
+    type: 'loop',
+    autoWidth: true,
+    autoHeight: true,
+    gap: '1rem',
+    arrows: false,
+  }).mount({ AutoScroll });
+});
