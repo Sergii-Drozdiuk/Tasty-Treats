7931e08936519d793b13ba480dc2196174728c64
chertik77 <babychdenya@gmail.com>
2023-09-14 19:09:51 +0100

    update

--- src/js/hero/hero-service.js
+++ src/js/hero/hero-service.js
@@ -1,11 +1,12 @@
 import JustValidate from 'just-validate';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
 import { uploadUser } from './hero-api';
 
 const form = document.querySelector('#order-form');
-const { name, phone, email, comment } = form.elements;
-
+const modalWindow = document.querySelector('#ordernow');
 const validator = new JustValidate(form);
+const { name, phone, email, comment } = form.elements;
 
 validator.addField(name, [
   { rule: 'required' },
@@ -17,22 +18,36 @@ validator.addField(phone, [{ rule: 'required' }, { rule: 'customRegexp', value: 
 
 validator.addField(email, [{ rule: 'required' }, { rule: 'email' }]);
 
-const modalWindow = document.querySelector('#ordernow');
+document.querySelector('.hero-order-btn').addEventListener('click', onOrderBtnClick);
+document.querySelector('.modal-order-send').addEventListener('click', onOrderSendBtnClick);
+document.querySelector('.btn-close').addEventListener('click', onCloseBtnClick);
+document.addEventListener('click', onBackdropClick);
 
-document.querySelector('.hero-order-btn').addEventListener('click', () => {
+function onOrderBtnClick() {
+  disablePageScroll();
   modalWindow.showModal();
   validator.refresh();
-});
+}
 
-document.querySelector('.btn-close').addEventListener('click', () => {
+function onOrderSendBtnClick() {
+  validator.revalidate().then(isFormValid);
+}
+
+function onCloseBtnClick() {
   modalWindow.close();
   form.reset();
   validator.destroy();
-});
+  enablePageScroll();
+}
 
-document.querySelector('.modal-order-send').addEventListener('click', () => {
-  validator.revalidate().then(isFormValid);
-});
+function onBackdropClick(e) {
+  if (e.target === modalWindow) {
+    modalWindow.close();
+    form.reset();
+    validator.destroy();
+    enablePageScroll();
+  }
+}
 
 function isFormValid(isValid) {
   if (isValid) {
@@ -52,9 +67,12 @@ function isFormValid(isValid) {
 
 function onOrderSuccess() {
   modalWindow.close();
+  enablePageScroll();
   form.reset();
   validator.destroy();
   return Notify.success('Order added successfully!');
 }
 
-const onOrderFailure = () => Notify.failure('Oops! Something went wrong, please try again.');
+function onOrderFailure() {
+  return Notify.failure('Oops! Something went wrong, please try again.');
+}
