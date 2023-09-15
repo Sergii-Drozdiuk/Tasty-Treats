6d55d692ab30a7a8c91e43e62b0dd04278bf70d9
AVasilaki <poseydon463@gmail.com>
2023-09-15 15:15:41 +0300

    upd

--- src/js/favorites/favorites-filters-api.js
+++ src/js/favorites/favorites-filters-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 console.log(axios);
+const categorys = ['Breakfast', 'Chicken'];
+console.log(categorys);
 const filters = document.querySelector('.favorites-filters-list');
 console.log(filters);
 filters.addEventListener('click', handlerFilter);
@@ -7,3 +9,14 @@ function handlerFilter(evt) {
   const categoryName = evt.target.textContent;
   console.log(categoryName);
 }
+const render = categorys
+  .map(
+    category =>
+      `<li>
+              <button type="button" class="favorites-filters-btn ">${category}</button>
+            </li>`
+  )
+  .join('');
+console.log(render);
+
+filters.insertAdjacentHTML('beforeend', render);
