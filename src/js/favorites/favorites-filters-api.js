d2ae924f3d6dabbb4ab357dbece6d837693c8598
AVasilaki <poseydon463@gmail.com>
2023-09-15 09:33:46 +0300

    upd

--- src/js/favorites/favorites-filters-api.js
+++ src/js/favorites/favorites-filters-api.js
@@ -1,2 +1,9 @@
 import axios from 'axios';
 console.log(axios);
+const filters = document.querySelector('.favorites-filters-list');
+console.log(filters);
+filters.addEventListener('click', handlerFilter);
+function handlerFilter(evt) {
+  const categoryName = evt.target.textContent;
+  console.log(categoryName);
+}
