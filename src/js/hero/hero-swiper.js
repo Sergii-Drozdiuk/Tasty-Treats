b89c2afdf7a2ad8f4b30722d15316f72e60b1df4
chertik77 <babychdenya@gmail.com>
2023-09-15 12:57:39 +0100

    update

--- src/js/hero/hero-swiper.js
+++ src/js/hero/hero-swiper.js
@@ -1,21 +1,60 @@
-import Splide from '@splidejs/splide';
-import '@splidejs/splide/css';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new Splide('.splide', {
-    type: 'loop',
-    autoWidth: true,
-    drag: false,
-    autoHeight: true,
-    gap: '1rem',
-    arrows: false,
-    perPage: 3,
-    autoplay: true,
-    pauseOnHover: true,
-    pauseOnFocus: true,
-    classes: {
-      pagination: 'splide__pagination pag-list',
-      page: 'splide__pagination__page pag-btn',
+new Swiper('.swiper', {
+  slidesPerView: 0.6,
+  spaceBetween: '16px',
+  loop: true,
+  modules: [Pagination],
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.pages-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 0.76,
     },
-  }).mount();
+    427: {
+      slidesPerView: 'auto',
+    },
+    530: {
+      slidesPerView: 1.1,
+    },
+    588: {
+      slidesPerView: 1.2,
+    },
+    640: {
+      slidesPerView: 1.3,
+    },
+    697: {
+      slidesPerView: 1.4,
+    },
+    746: {
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 0.9,
+    },
+    900: {
+      slidesPerView: 1.1,
+    },
+    997: {
+      slidesPerView: 1.2,
+    },
+    1084: {
+      slidesPerView: 1.3,
+    },
+    1175: {
+      slidesPerView: 1.4,
+    },
+    1200: {
+      slidesPerView: 0.97,
+    },
+  },
 });
