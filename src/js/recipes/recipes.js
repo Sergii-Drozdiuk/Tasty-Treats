13734100c0c418c9760d520649c76812fae2a47a
Valentun2 <pocomaxawrkt@gmail.com>
2023-09-16 13:02:10 +0300

    Update recipes.js

--- src/js/recipes/recipes.js
+++ src/js/recipes/recipes.js
@@ -22,7 +22,7 @@ getFetchRecipes()
   return `
   <li class ="recipes-card" style="background-image:linear-gradient(to top, var(--main-text-dark-color),var( --filters-main-color),transparent 100%), url(${preview});" data-id="${_id}">
   <svg class="recipes-icon-heart" width="22" height="22">
-      <use href="./img/icons.svg#icon-heart">
+      <use href="img/icons.svg#icon-heart">
       </use>
     </svg>
 <h3 class="recipes-title">${title.slice(0,22)}</h3>
@@ -31,23 +31,23 @@ getFetchRecipes()
 <div class="rating-wraper">
   <p class="recipes-text-rating">4.5</p>
   <svg class="recipes-icon-svg" width="18" height="18">
-      <use href="./img/icons.svg#icon-star">
+      <use href="img/icons.svg#icon-star">
       </use>
     </svg>
     <svg class="recipes-icon-svg" width="18" height="18">
-      <use href="./img/icons.svg#icon-star">
+      <use href="img/icons.svg#icon-star">
       </use>
     </svg>
     <svg class="recipes-icon-svg" width="18" height="18">
-      <use href="./img/icons.svg#icon-star">
+      <use href="img/icons.svg#icon-star">
       </use>
     </svg>
     <svg class="recipes-icon-svg" width="18" height="18">
-      <use href="./img/icons.svg#icon-star">
+      <use href="img/icons.svg#icon-star">
       </use>
     </svg>
     <svg class="recipes-icon-svg" width="18" height="18">
-      <use href="./img/icons.svg#icon-star">
+      <use href="img/icons.svg#icon-star">
       </use>
     </svg>
     </div>
