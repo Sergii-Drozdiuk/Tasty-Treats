b1ed19bedb3af3961d8be0ae3074546c97f21ad4
chertik7777 <144826348+chertik7777@users.noreply.github.com>
2023-09-17 14:05:39 +0000

    Deploying to gh-pages from @ Sergii-Drozdiuk/Fancy-team-project@2dfc5bfe1a8d549e06111c952dd32b0a803525aa 🚀

--- assets/favorites-1970afbb.js
+++
@@ -1,3 +0,0 @@
-import{a as l}from"./axios-41df7738.js";console.log(l);const o=["Breakfast","Chicken","Dessert"];console.log(o);const t=document.querySelector(".favorites-filters-list");console.log(t);t.addEventListener("click",r);function r(e){const s=e.target.textContent;console.log(s)}const n=o.map(e=>`<li>
-              <button type="button" class="favorites-filters-btn ">${e}</button>
-            </li>`).join("");console.log(n);t.insertAdjacentHTML("beforeend",n);
