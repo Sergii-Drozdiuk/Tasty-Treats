ade2b4256357f9786b3cd4e911ba643c4d4f5663
Sergii-Drozdiuk <126243923+Sergii-Drozdiuk@users.noreply.github.com>
2023-09-18 17:44:49 +0000

    Deploying to gh-pages from @ Sergii-Drozdiuk/Fancy-team-project@b1aec56a9bd0085f5429b2091a8c9aa857884248 🚀

--- assets/favorites-d5550614.js
+++
@@ -1,3 +0,0 @@
-import{a as t}from"./header-f48477e9.js";console.log(t);const n=["Breakfast","Chicken","Dessert"];console.log(n);const o=document.querySelector(".favorites-filters-list");console.log(o);o.addEventListener("click",r);function r(e){const l=e.target.textContent;console.log(l)}const s=n.map(e=>`<li>
-              <button type="button" class="favorites-filters-btn ">${e}</button>
-            </li>`).join("");console.log(s);o.insertAdjacentHTML("beforeend",s);console.log(t);
