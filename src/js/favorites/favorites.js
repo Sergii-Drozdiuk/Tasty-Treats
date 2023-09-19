2c1d3526f9a911f6e23d18e0087eb52462e54b5d
AVasilaki <poseydon463@gmail.com>
2023-09-19 22:20:54 +0300

    upd

--- src/js/favorites/favorites.js
+++ src/js/favorites/favorites.js
@@ -1,1 +1,32 @@
+const filters = document.querySelector('.favorites-filters-list');
 
+const sevedRecipes = JSON.parse(localStorage.getItem('favorite-recipes'));
+console.log(sevedRecipes);
+if (sevedRecipes) {
+  filters.classList.remove('js-hidden');
+}
+const categorys = [
+  {
+    tags: 'Beef',
+    id: 1,
+  },
+];
+filters.addEventListener('click', handlerFilter);
+function handlerFilter(evt) {
+  const categoryName = evt.target.textContent;
+  console.log(categoryName);
+  categorys.filter(category => category.tags === categoryName).map(({ id }) => console.log(id));
+}
+function renderingBtn(arr) {
+  const render = arr
+    .map(
+      ({ tags }) =>
+        `<li>
+              <button type="button" class="favorites-filters-btn ">${tags}</button>
+            </li>`
+    )
+    .join('');
+
+  filters.insertAdjacentHTML('beforeend', render);
+}
+renderingBtn(categorys);
