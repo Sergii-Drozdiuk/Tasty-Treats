a4565bdc82682978702675c5fd687fef1452ab3c
katusha4leon <katusha4leon@gmail.com>
2023-09-19 23:56:36 +0300

    update styles

--- src/js/pop-recipes/pop-recipes.js
+++ src/js/pop-recipes/pop-recipes.js
@@ -6,7 +6,7 @@ export function displayData(dataArray) {
     const markup = `
       <li>
         <a class="pop-item" id="${_id}">
-          <img class="pop-img" src="${preview}" alt="${title}">
+          <img class="pop-img" width="64" height="64" loading="lazy" src="${preview}" alt="${title}">
           <div class="pop-description">
           <h3 class="pop-description-title">${title}</h3>
           <p class="pop-description-text">${description}</p>
