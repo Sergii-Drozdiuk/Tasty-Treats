274721ce4835302748bb89ab7cb53c687b44549d
AVasilaki <poseydon463@gmail.com>
2023-09-20 07:40:04 +0300

    upd

--- src/js/favorites/favorites.js
+++ src/js/favorites/favorites.js
@@ -1,22 +1,31 @@
 const filters = document.querySelector('.favorites-filters-list');
-
+const hat = document.querySelector('.hat-wrapper');
 const sevedRecipes = JSON.parse(localStorage.getItem('favorite-recipes'));
-console.log(sevedRecipes);
+
+console.log(filters.children);
 if (sevedRecipes) {
   filters.classList.remove('js-hidden');
+  hat.classList.add('js-hidden');
 }
 const categorys = [
   {
     tags: 'Beef',
     id: 1,
   },
+  {
+    tags: 'Desert',
+    id: 2,
+  },
 ];
+
 filters.addEventListener('click', handlerFilter);
+
 function handlerFilter(evt) {
   const categoryName = evt.target.textContent;
   console.log(categoryName);
   categorys.filter(category => category.tags === categoryName).map(({ id }) => console.log(id));
 }
+
 function renderingBtn(arr) {
   const render = arr
     .map(
