573fb962cdaa8c500e546300545d8951aab91696
Valentun2 <pocomaxawrkt@gmail.com>
2023-09-21 14:22:11 +0300

    Update recipe-localstorage.js

--- src/js/recipes/recipe-localstorage.js
+++ src/js/recipes/recipe-localstorage.js
@@ -2,8 +2,21 @@ export function afterCardLoaded(r) {
   const favoritesBtn = document.querySelector('.favorites-btn');
   const removeBtn = document.querySelector('.remove-btn');
 
+  const i ={ id: r._id, tags: r.tags };
+ 
   favoritesBtn.addEventListener('click', () => {
-    localStorage.setItem('favorite-recipes', JSON.stringify({ id: r._id, tags: r.tags }));
+
+
+    for (const obj of arr) {
+        if (r._id === obj.id) {
+
+            return;
+          }
+    }
+    arr.push(i);
+
+    
+    localStorage.setItem('favorite-recipes', JSON.stringify(arr));
     favoritesBtn.style.display = 'none';
     removeBtn.style.display = 'inline-block';
 
@@ -12,19 +25,22 @@ export function afterCardLoaded(r) {
       removeBtn.style.display = 'inline-block';
     }
 
-    removeBtn.addEventListener('click', () => {
-      localStorage.removeItem(r.title);
+    removeBtn.addEventListener('click', (evt) => {
+        arr.map(obj => {
+            console.log(evt.target.id);
+            console.log(obj.id);
+            if (r._id === obj.id) {
+              arr.splice(arr.indexOf(obj), 1);
+             
+            }
+          });
+
+          localStorage.setItem('favorite-recipes', JSON.stringify(arr));
+      
       removeBtn.style.display = 'none';
       favoritesBtn.style.display = 'inline-block';
     });
 
-    const data = JSON.parse(localStorage.getItem('favorite-recipes'));
-
-    if (data._id !== r._id) {
-      localStorage.setItem('favorite-recipes', JSON.stringify({ id: r._id, tags: r.tags }));
-    } else {
-      return;
-    }
   });
 }
 
@@ -37,13 +53,10 @@ const arr = JSON.parse(localStorage.getItem('favorite-recipes')) ?? [];
 function hadlerClick(evt) {
   const i = {
     id: evt.target.id,
-    tags: Object.values(evt.target.dataset),
+    tags: Object.values(evt.target.dataset).join(',').split(','),
   };
 
-  if (
-    evt.target.classList.contains('path') &&
-    evt.target.farthestViewportElement.classList.contains('heart-active')
-  ) {
+  if ( evt.target.classList.contains('path') &&  evt.target.farthestViewportElement.classList.contains('heart-active')) {
     evt.target.farthestViewportElement.classList.remove('heart-active');
     arr.map(obj => {
       console.log(evt.target.id);
@@ -58,9 +71,17 @@ function hadlerClick(evt) {
   }
 
   if (evt.target.classList.contains('recipes-icon-heart') && !evt.target.classList.contains('heart-active')) {
-    arr.push(i);
-    evt.target.classList.add('heart-active');
-    localStorage.setItem('favorite-recipes', JSON.stringify(arr));
-    return;
+      evt.target.classList.add('heart-active');
+      
+      for (const obj of arr) {
+          console.log(evt.target.id === obj.id);
+          if (evt.target.id === obj.id) {
+              return;
+            }
+      }
+      arr.push(i);
   }
+  localStorage.setItem('favorite-recipes', JSON.stringify(arr));
+
+  console.log(i);
 }
