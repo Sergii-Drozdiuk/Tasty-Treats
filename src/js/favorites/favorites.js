f6880bf2f5e778adafc2f65785afb48de8f9c970
AVasilaki <poseydon463@gmail.com>
2023-09-22 01:28:00 +0300

    Update favorites.js

--- src/js/favorites/favorites.js
+++ src/js/favorites/favorites.js
@@ -2,30 +2,29 @@ const filters = document.querySelector('.favorites-filters-list');
 const hat = document.querySelector('.hat-wrapper');
 const sevedRecipes = JSON.parse(localStorage.getItem('favorite-recipes'));
 
-console.log(filters.children);
 if (sevedRecipes) {
   filters.classList.remove('js-hidden');
   hat.classList.add('js-hidden');
 }
-const categorys = [
-  {
-    tags: ['Beef', 'Soup'],
-    id: 1,
-  },
-  {
-    tags: ['Desert'],
-    id: 2,
-  },
-  {
-    tags: ['Beef'],
-    id: 3,
-  },
-  {
-    tags: [],
-    id: 4,
-  },
-];
-const allTags = categorys.flatMap(category => category.tags);
+// const categorys = [
+//   {
+//     tags: ['Beef', 'Soup'],
+//     id: 1,
+//   },
+//   {
+//     tags: ['Desert'],
+//     id: 2,
+//   },
+//   {
+//     tags: ['Beef'],
+//     id: 3,
+//   },
+//   {
+//     tags: [],
+//     id: 4,
+//   },
+// ];
+const allTags = sevedRecipes.flatMap(category => category.tags);
 const unicTags = allTags.filter((tag, i, arr) => arr.indexOf(tag) === i);
 
 filters.addEventListener('click', handlerFilter);
@@ -33,7 +32,7 @@ filters.addEventListener('click', handlerFilter);
 function handlerFilter(evt) {
   const categoryName = evt.target.textContent;
   console.log(categoryName);
-  categorys.filter(category => category.tags === categoryName).map(({ id }) => console.log(id));
+  sevedRecipes.filter(category => category.tags === categoryName).map(({ id }) => console.log(id));
 }
 
 function renderingBtn(arr) {
