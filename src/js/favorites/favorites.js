c7ca301ecb6ca1ea50894d8efef596d2f49ec0bd
AVasilaki <poseydon463@gmail.com>
2023-09-22 12:12:04 +0300

    upd

--- src/js/favorites/favorites.js
+++ src/js/favorites/favorites.js
@@ -1,14 +1,20 @@
+import { createMarcup } from '../recipes/markups/recipes-main-markup.js';
+import { getResById } from './favorites-recipes-api.js';
+const arrOfRecepis = [];
+console.log(arrOfRecepis, 'arr');
+
+const container = document.querySelector('.favorites-cards');
 const filters = document.querySelector('.favorites-filters-list');
 const hat = document.querySelector('.hat-wrapper');
 const sevedRecipes = JSON.parse(localStorage.getItem('favorite-recipes'));
-
+// console.log(sevedRecipes);
 if (sevedRecipes) {
   filters.classList.remove('js-hidden');
   hat.classList.add('js-hidden');
 }
 
 const allTags = sevedRecipes.flatMap(category => category.tags);
-const unicTags = allTags.filter((tag, i, arr) => arr.indexOf(tag) === i);
+const unicTags = allTags.filter((tag, i, arr) => arr.indexOf(tag) === i).filter(tag => tag !== '');
 
 filters.addEventListener('click', handlerFilter);
 
@@ -30,4 +36,18 @@ function renderingBtn(arr) {
 
   filters.insertAdjacentHTML('beforeend', render);
 }
+
 renderingBtn(unicTags);
+
+function renderingRecipe(arr) {
+  arr.map(({ id }) =>
+    getResById(id).then(r => {
+      console.log(r);
+      arrOfRecepis.push(r);
+    })
+  );
+  // console.log(render);
+}
+
+renderingRecipe(sevedRecipes);
+container.insertAdjacentHTML('beforeend', createMarcup(arrOfRecepis));
